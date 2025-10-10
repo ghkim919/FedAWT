7e1392ddd77135964203056500ca464759480b84
hamji <ghkim919@mju.ac.kr>
2025-10-10 18:22:38 +0900

    up to dated

--- simulation/config_reader.py
+++ simulation/config_reader.py
@@ -6,8 +6,5 @@ class ConfigReader:
 		self.file_path = file_path
 
 	def read(self) -> Dict[str, Any]:
-		"""
-		JSON 파일을 읽어서 dictionary 형태로 반환합니다.
-		"""
 		with open(self.file_path, 'r', encoding='utf-8') as f:
 			return json.load(f)
